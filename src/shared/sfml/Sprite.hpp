5257a2eaacdfca02fcb5c1471ebaf7f55cb5f367
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-04-12 01:05:56 +0200

    :sparkles: Enhance Sprite and SpriteSheet classes with new methods for sprite management

--- src/shared/sfml/Sprite.hpp
+++ src/shared/sfml/Sprite.hpp
@@ -13,11 +13,15 @@ private:
 	sf::Sprite _sprite;
 public:
    	void display(sf::RenderWindow &window);
+
 	void changePos(sf::Vector2f pos);
+
 	sf::Vector2f getPos();
 
 	Sprite(const std::string &path);
+
 	Sprite(const std::string &path, sf::Vector2f pos, sf::Vector2f scale);
+
     ~Sprite() = default;
 };
 
