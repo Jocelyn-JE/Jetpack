19be7a8e4996ce11ff471c877a9be56efef406da
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-04-15 14:27:52 +0200

    🎨 Use this in class for clearer code

--- src/server/Packet.cpp
+++ src/server/Packet.cpp
@@ -30,69 +30,63 @@ Payload_t Packet::createPayloadHeader(uint16_t dataCount, uint8_t dataId) {
 
 Packet::Packet(uint8_t nbrPayload) {
     uint16_t headerBigEndian = htons(createPacketHeader(nbrPayload).rawData);
-    _packet.push_back(static_cast<uint8_t>(headerBigEndian >> 8));
-    _packet.push_back(static_cast<uint8_t>(headerBigEndian & 0xFF));
+    this->_packet.push_back(static_cast<uint8_t>(headerBigEndian >> 8));
+    this->_packet.push_back(static_cast<uint8_t>(headerBigEndian & 0xFF));
 }
 
 void Packet::addPayloadHeader(uint16_t dataCount, uint8_t dataId) {
     uint16_t payload = htons(createPayloadHeader(dataCount, dataId).rawData);
-    _packet.push_back(static_cast<uint8_t>(payload >> 8));
-    _packet.push_back(static_cast<uint8_t>(payload & 0xFF));
+    this->_packet.push_back(static_cast<uint8_t>(payload >> 8));
+    this->_packet.push_back(static_cast<uint8_t>(payload & 0xFF));
 }
 
 void Packet::addData(uint32_t data) {
     uint32_t bigEndianData = htonl(data);
-    _packet.push_back(static_cast<uint8_t>(bigEndianData >> 24));
-    _packet.push_back(static_cast<uint8_t>(bigEndianData >> 16));
-    _packet.push_back(static_cast<uint8_t>(bigEndianData >> 8));
-    _packet.push_back(static_cast<uint8_t>(bigEndianData & 0xFF));
+    this->_packet.push_back(static_cast<uint8_t>(bigEndianData >> 24));
+    this->_packet.push_back(static_cast<uint8_t>(bigEndianData >> 16));
+    this->_packet.push_back(static_cast<uint8_t>(bigEndianData >> 8));
+    this->_packet.push_back(static_cast<uint8_t>(bigEndianData & 0xFF));
 }
 
 void Packet::addData(uint16_t data) {
     uint16_t bigEndianData = htons(data);
-    _packet.push_back(static_cast<uint8_t>(bigEndianData >> 8));
-    _packet.push_back(static_cast<uint8_t>(bigEndianData & 0xFF));
+    this->_packet.push_back(static_cast<uint8_t>(bigEndianData >> 8));
+    this->_packet.push_back(static_cast<uint8_t>(bigEndianData & 0xFF));
 }
 
 void Packet::addData(uint8_t data) {
-    _packet.push_back(static_cast<uint8_t>(data & 0xFF));
+    this->_packet.push_back(static_cast<uint8_t>(data & 0xFF));
 }
 
 void Packet::add(uint32_t data, PayloadType_t type) {
-    addPayloadHeader(1, type);
-    addData(data);
+    this->addPayloadHeader(1, type);
+    this->addData(data);
 }
 
 void Packet::add(uint16_t data, PayloadType_t type) {
-    addPayloadHeader(1, type);
-    addData(data);
+    this->addPayloadHeader(1, type);
+    this->addData(data);
 }
 
 void Packet::add(uint8_t data, PayloadType_t type) {
-    addPayloadHeader(1, type);
-    addData(data);
+    this->addPayloadHeader(1, type);
+    this->addData(data);
 }
 
-void Packet::add(std::vector<uint32_t> data, PayloadType_t type) {
-    addPayloadHeader(data.size(), type);
-    for (const auto& d : data) {
-        addData(d);
-    }
+void Packet::add(std::vector<uint32_t> dataList, PayloadType_t type) {
+    this->addPayloadHeader(dataList.size(), type);
+    for (const auto& data : dataList) this->addData(data);
 }
 
-void Packet::add(std::vector<uint16_t> data, PayloadType_t type) {
-    addPayloadHeader(data.size(), type);
-    for (const auto& d : data) {
-        addData(d);
-    }
+void Packet::add(std::vector<uint16_t> dataList, PayloadType_t type) {
+    this->addPayloadHeader(dataList.size(), type);
+    for (const auto& data : dataList) this->addData(data);
 }
 
-void Packet::add(std::vector<uint8_t> data, PayloadType_t type) {
-    addPayloadHeader(1, type);
-    addPayloadHeader(data.size(), type);
-    for (const auto& d : data) {
-        addData(d);
-    }
+void Packet::add(std::vector<uint8_t> dataList, PayloadType_t type) {
+    this->addPayloadHeader(1, type);
+    this->addPayloadHeader(dataList.size(), type);
+    for (const auto& data : dataList) this->addData(data);
 }
 
 const std::vector<uint8_t>& Packet::getPacket() const { return _packet; }
