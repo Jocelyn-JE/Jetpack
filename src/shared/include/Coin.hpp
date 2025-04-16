4a0062a749f3fde9c021b60035633c6a51cac967
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-04-16 14:04:08 +0200

    🚨 Fixed linter error

--- src/shared/include/Coin.hpp
+++ src/shared/include/Coin.hpp
@@ -5,5 +5,6 @@ typedef struct coinsPos_s {
     int y_pos;
     size_t coinId;
 
-    coinsPos_s(int x = 0, int y = 0, size_t id = 0) : x_pos(x), y_pos(y), coinId(id) {}
+    coinsPos_s(int x = 0, int y = 0, size_t id = 0)
+        : x_pos(x), y_pos(y), coinId(id) {}
 } coinsPos_t;
