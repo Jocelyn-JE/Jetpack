9b103306ceed574a31c571a5f3a33e1314e6b6ed
MatthieuGA <matthieu.guillet-arnaud@epitech.eu>
2025-04-16 10:36:56 +0200

    :sparkles: coin id added

--- src/shared/include/Coin.hpp
+++ src/shared/include/Coin.hpp
@@ -3,7 +3,7 @@
 typedef struct coinsPos_s {
     int x_pos;
     int y_pos;
-    int coinId;
+    size_t coinId;
 
-    coinsPos_s(int x = 0, int y = 0) : x_pos(x), y_pos(y) {}
+    coinsPos_s(int x = 0, int y = 0, size_t id = 0) : x_pos(x), y_pos(y), coinId(id) {}
 } coinsPos_t;
